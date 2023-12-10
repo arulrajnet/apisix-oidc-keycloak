fbe77990fc6cd46a5c782c13ebe77a5b9b6b6446
Arul <me@arulraj.net>
2023-12-10 18:43:33 +0530

    Fix the discovery failure if the route is /* by route priority and exact hostnames for keycloak routes.
    The exact issue is "openidc.lua:573: openidc_discover(): accessing discovery url (http://foo.example.com/auth/realms/foo/.well-known/openid-configuration) failed: timeout,"
    
    - Different redirect_uri to fix "OIDC authentication failed: request to the redirect_uri path but there's no session state found"

--- apisix_conf/apisix-config-init.sh
+++ apisix_conf/apisix-config-init.sh
@@ -57,59 +57,67 @@ curl $CURL_OPTS -X PUT -H "X-API-KEY: ${APISIX_SERVICE_ADMIN_SECRET}" http://${A
 
 ### Keycloak
 
-curl $CURL_OPTS -X PUT -H "X-API-KEY: ${APISIX_SERVICE_ADMIN_SECRET}" http://${APISIX_SERVICE_HOST}:${APISIX_SERVICE_ADMIN_PORT}/apisix/admin/routes/11 -d "
+curl $CURL_OPTS -X PUT -H "X-API-KEY: ${APISIX_SERVICE_ADMIN_SECRET}" http://${APISIX_SERVICE_HOST}:${APISIX_SERVICE_ADMIN_PORT}/apisix/admin/routes/11 -d '
 {
-    \"uris\": [\"/auth/*\", \"/auth/\"],
-    \"service_id\": \"1\",
-    \"hosts\": [\"*.${BASE_FQDN}\"],
-    \"methods\": [\"GET\",\"POST\",\"PUT\",\"DELETE\"],
-    \"priority\": 999
-}"
+    "uris": ["/auth/*", "/auth/"],
+    "service_id": "1",
+    "hosts": ["'"foo.${BASE_FQDN}"'","'"bar.${BASE_FQDN}"'"],
+    "methods": ["GET","POST","PUT","DELETE"],
+    "priority": 999
+}'
 
 ### Whoami
 
-curl $CURL_OPTS -X PUT -H "X-API-KEY: ${APISIX_SERVICE_ADMIN_SECRET}" http://${APISIX_SERVICE_HOST}:${APISIX_SERVICE_ADMIN_PORT}/apisix/admin/routes/12 -d "
+curl $CURL_OPTS -X PUT -H "X-API-KEY: ${APISIX_SERVICE_ADMIN_SECRET}" http://${APISIX_SERVICE_HOST}:${APISIX_SERVICE_ADMIN_PORT}/apisix/admin/routes/12 -d '
 {
-    \"uris\": [\"/whoami/*\", \"/\"],
-    \"service_id\": \"2\",
-    \"hosts\": [\"foo.${BASE_FQDN}\"],
-    \"methods\": [\"GET\",\"POST\",\"PUT\",\"DELETE\"],
-    \"priority\": 10,
-    \"plugins\":{
-        \"openid-connect\":{
-            \"client_id\": \"foo\",
-            \"client_secret\": \"${FOO_CLIENT_SECRET}\",
-            \"discovery\": \"http://foo.${BASE_FQDN}/auth/realms/foo/.well-known/openid-configuration\",
-            \"scope\": \"openid profile\",
-            \"bearer_only\": false,
-            \"realm\": \"foo\",
-            \"introspection_endpoint_auth_method\": \"client_secret_post\",
-            \"redirect_uri\": \"http://foo.${BASE_FQDN}/\",
-            \"logout_path\": \"/whoami/logout\",
-            \"post_logout_redirect_uri\": \"http://foo.${BASE_FQDN}/whoami/\"
+    "uris": ["/*"],
+    "service_id": "2",
+    "hosts": ["'"foo.${BASE_FQDN}"'"],
+    "methods": ["GET","POST","PUT","DELETE"],
+    "priority": 10,
+    "plugins":{
+        "openid-connect":{
+            "bearer_only": false,
+            "client_id": "foo",
+            "client_secret": "'"${FOO_CLIENT_SECRET}"'",
+            "discovery": "'"http://foo.${BASE_FQDN}/auth/realms/foo/.well-known/openid-configuration"'",
+            "introspection_endpoint_auth_method": "client_secret_post",
+            "logout_path": "/logout",
+            "post_logout_redirect_uri": "'"http://foo.${BASE_FQDN}/"'",
+            "realm": "foo",
+            "redirect_uri": "'"http://foo.${BASE_FQDN}/callback"'",
+            "scope": "openid profile email",
+            "set_refresh_token_header": true,
+            "session": {
+                "secret": "'"${SESSION_SECRET}"'"
+            }
         }
     }
-}"
+}'
 
-curl $CURL_OPTS -X PUT -H "X-API-KEY: ${APISIX_SERVICE_ADMIN_SECRET}" http://${APISIX_SERVICE_HOST}:${APISIX_SERVICE_ADMIN_PORT}/apisix/admin/routes/13 -d "
+curl $CURL_OPTS -X PUT -H "X-API-KEY: ${APISIX_SERVICE_ADMIN_SECRET}" http://${APISIX_SERVICE_HOST}:${APISIX_SERVICE_ADMIN_PORT}/apisix/admin/routes/13 -d '
 {
-    \"uris\": [\"/whoami/*\", \"/\"],
-    \"service_id\": \"2\",
-    \"hosts\": [\"bar.${BASE_FQDN}\"],
-    \"methods\": [\"GET\",\"POST\",\"PUT\",\"DELETE\"],
-    \"priority\": 10,
-    \"plugins\":{
-        \"openid-connect\":{
-            \"client_id\": \"bar\",
-            \"client_secret\": \"${BAR_CLIENT_SECRET}\",
-            \"discovery\": \"http://bar.${BASE_FQDN}/auth/realms/bar/.well-known/openid-configuration\",
-            \"scope\": \"openid profile\",
-            \"bearer_only\": false,
-            \"realm\": \"bar\",
-            \"introspection_endpoint_auth_method\": \"client_secret_post\",
-            \"redirect_uri\": \"http://bar.${BASE_FQDN}/\",
-            \"logout_path\": \"/whoami/logout\",
-            \"post_logout_redirect_uri\": \"http://foo.${BASE_FQDN}/whoami/\"
+    "uris": ["/*"],
+    "service_id": "2",
+    "hosts": ["'"bar.${BASE_FQDN}"'"],
+    "methods": ["GET","POST","PUT","DELETE"],
+    "priority": 20,
+    "plugins":{
+        "openid-connect":{
+            "bearer_only": false,
+            "client_id": "bar",
+            "client_secret": "'"${BAR_CLIENT_SECRET}"'",
+            "discovery": "'"http://bar.${BASE_FQDN}/auth/realms/bar/.well-known/openid-configuration"'",
+            "introspection_endpoint_auth_method": "client_secret_post",
+            "logout_path": "/logout",
+            "post_logout_redirect_uri": "'"http://bar.${BASE_FQDN}/"'",
+            "realm": "bar",
+            "redirect_uri": "'"http://bar.${BASE_FQDN}/callback"'",
+            "scope": "openid profile email",
+            "set_refresh_token_header": true,
+            "session": {
+                "secret": "'"${SESSION_SECRET}"'"
+            }
         }
     }
-}"
+}'
